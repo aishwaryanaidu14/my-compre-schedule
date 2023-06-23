a2c00fe3493ab6cc83bedd9dda183257a485a2ae
aishwaryanaidu14 <75825070+aishwaryanaidu14@users.noreply.github.com>
2023-06-23 05:37:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+phyLabEl=document.getElementById("phy-lab")
+mybtn=document.getElementById("btn")
+
+mybtn.addEventListener("click", function(){
+	phyLabEl.textContent="PHYSICS LAB IS MY FAVOURITE COURSE"
+	phyLabEl.style.backgroundColor="#80ffff"
+})
+
